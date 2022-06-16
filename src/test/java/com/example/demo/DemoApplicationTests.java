02ee660d279d6dec14cfd0b55598145d4d796799
sharu-91 <sharan.9114@gmail.com>
2022-06-17 01:11:32 +0530

    latest

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		String response = DemoApplication.hello("sharan");
+		System.out.println("Response of the name " + response);
+		System.out.println("Response of the name " + response.equals("Hello Sharan!"));
+		//comment to gitgit 
+	}
+
+}
